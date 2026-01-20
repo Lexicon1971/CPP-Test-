81e1a3109655a7a0bcb1310b5784ef066fe1c04e
lexicon1971 <lexicon.sm@gmail.com>
2026-01-20 09:49:02 +0200

    Force vite to bundle index.tsx into javascript

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Change this to a dot-slash
+  base: './', 
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: './index.html',
