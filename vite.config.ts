b0f6cf5915fda159e4e52dde1d49c206f4707abd
lexicon1971 <lexicon.sm@gmail.com>
2026-01-20 07:56:32 +0200

    Force relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CPP-Test-/',
+  base: './', // Change this to a dot-slash
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: './index.html',
